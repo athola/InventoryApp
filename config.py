ac9ddc48746fc532427a2d9ddbd21b67f9adc60e
Alex <alex@uprev.com>
2019-07-30 12:16:36 -0500

    adding pipenv and config file, updating app to use APP_SETTINGS

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object)
+	DEBUG = False
+	TESTING = False
+	CSRF_ENABLED = True
+	SECRET_KEY = "f95b0391fc26cddb3b563fab72fa7a4f3fa5f29d29e18a4b4fbd98125d8b39f8"
+	
+class ProductionConfig(Config):
+	DEBUG = False
+	
+class StagingConfig(Config):
+	DEVELOPMENT = True
+	DEBUG = True
+	
+class TestingConfig(Config):
+	TESTING = True
--- inventoryApp.py
+++ inventoryApp.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask
 inventoryApp = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @inventoryApp.route('/')
 def hello():
