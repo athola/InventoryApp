04ade2621295fd5edbb16af6a6db32055fae6f0f
Alex <alex@uprev.com>
2019-07-30 11:49:43 -0500

    adding name extension functionality to url

--- inventoryApp.py
+++ inventoryApp.py
@@ -4,6 +4,10 @@ inventoryApp = Flask(__name__)
 @inventoryApp.route('/')
 def hello():
 	return "Hello World!"
+	
+@inventoryApp.route('/<name>')
+def hello_name(name):
+	return "Hello {}!".format(name)
 
 if __name__ == '__main__':
 	inventoryApp.run()
