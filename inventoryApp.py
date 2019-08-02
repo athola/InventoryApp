05b2222c59b734332bd433e3a8863273ac9b40dc
Alex <alex@uprev.com>
2019-08-01 19:36:21 -0500

    adding updated app to stage

--- inventoryApp.py
+++ inventoryApp.py
@@ -1,6 +1,9 @@
-import os
-from flask import Flask
+import os, sys
+import requests
+import json
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exists
 
 inventoryApp = Flask(__name__)
 inventoryApp.config.from_object(os.environ['APP_SETTINGS'])
@@ -8,14 +11,108 @@ inventoryApp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(inventoryApp)
 
 from models import Grocery
-
-@inventoryApp.route('/')
-def hello():
-	return "Hello World!"
+@inventoryApp.route('/', methods=['GET', 'POST'])
+def index():
+	errors = []
+	items = []
+	col = "ID"
+	load_search = False
+	load_add_item = True
+	item_searched = False
+	item_added = False
+	if request.method == "POST":
+		if 'search-item' in request.form:
+			load_search = True
+			load_add_item = False
+			return render_template('index.html', errors = errors, items=items, column=col, loading_search=load_search, loading_add_item=load_add_item, item_searched=item_searched, item_added=item_added)
+		elif 'add-item' in request.form:
+			load_search = False
+			load_add_item = True
+			return render_template('index.html', errors = errors, items=items, column=col, loading_search=load_search, loading_add_item=load_add_item, item_searched=item_searched, item_added=item_added)
+		elif 'send-search' in request.form:
+			load_search = True
+			load_add_item = False
+			item_searched = True
+			item_added = False
+			try:
+				col = request.form['column']
+				item = request.form['item']
+				matching_items = get_matching_items(col, item)
+				for item in matching_items:
+					items.append(item)
+			except Exception as ex:
+				error_type = "Unable to search for item. Please double check your search parameters. "
+				errors = report_exception(ex, error_type, errors)
+		elif 'send-add' in request.form:
+			load_search = False
+			load_add_item = True
+			item_searched = False
+			try:
+				item = set_item()
+				errors, items = add_item(item, errors, items)
+				item_added = True
+			except Exception as ex:
+				error_type = "Unable to add item. Please double check your item parameters. "
+				errors = report_exception(ex, error_type, errors)
+	item_length = len(items)
+	return render_template('index.html', errors = errors, items=items, column=col, loading_search=load_search, loading_add_item=load_add_item, item_searched=item_searched, item_added=item_added, )
 	
-@inventoryApp.route('/<name>')
-def hello_name(name):
-	return "Hello {}!".format(name)
+def report_exception(ex, error_type, errors):
+	error_str = error_type + str(ex) + " - Error on line no: {}".format(sys.exc_info()[-1].tb_lineno)
+	errors.append(error_str)
+	for error in errors:
+		print (error)
+	return errors
 
-if __name__ == '__main__':
-	inventoryApp.run()
+def get_matching_items(search_column, search_item):
+	if search_column == "id":
+		if search_item.isdigit():
+			return Grocery.query.filter(Grocery.id == int(search_item))
+		else:
+			return {}
+	elif '*' in search_item or '_' in search_item: 
+		search_term = search_item.replace('_', '__')\
+							.replace('*', '%')\
+							.replace('?', '_')
+	elif search_item[-1] == 's':
+		search_term = search_item[0:len(search_item) - 1]
+		search_term = '%{0}%'.format(search_item)
+	else:
+		search_term = '%{0}%'.format(search_item)
+	return Grocery.query.filter(getattr(Grocery, search_column).ilike(search_term)).order_by(Grocery.id)
+
+def set_item():
+	id = int(request.form['id-add'])
+	description = request.form['description-add']
+	last_sold = request.form['last-sold-add']
+	shelf_life = request.form['shelf-life-add']
+	department = request.form['department-add']
+	price = request.form['price-add']
+	unit = request.form['unit-add']
+	x_for = request.form['xfor-add']
+	cost = request.form['cost-add']
+	item_to_add = Grocery(id=id,
+		description=description,
+		last_sold=last_sold,
+		shelf_life=shelf_life,
+		department=department,
+		price=price,
+		unit=unit,
+		x_for=x_for,
+		cost=cost,
+	)
+	return item_to_add
+
+def add_item(item, errors, items):
+	try:
+		item_exists = db.session.query(exists().where(Grocery.id==item.id)).scalar()
+		if not (item_exists):
+			db.session.add(item)
+			db.session.commit()
+			json_obj = json.dumps([dict(i) for i in item])
+			items.append(json_obj)
+		else:
+			errors.append("Unable to add item to database. This item has already been added with ID: " + str(item.id))
+	except Exception as ex:
+		errors.append("Unable to add item to database." + str(ex))
+	return errors, items
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Grocery(db.Model):
 	
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.String(60), nullable=False)
-	last_sold = db.Column(db.DateTime())
+	last_sold = db.Column(db.Date)
 	shelf_life = db.Column(db.String(5), nullable=False)
 	department = db.Column(db.String(40))
 	price = db.Column(db.String(20), nullable=False)
@@ -13,15 +13,13 @@ class Grocery(db.Model):
 	x_for = db.Column(db.Integer, nullable=False)
 	cost = db.Column(db.String(20), nullable=False)
 	
-	def __init__(self, description, last_sold, shelf_life, department, price, unit, x_for, cost):
+	def __init__(self, id, description, last_sold, shelf_life, department, price, unit, x_for, cost):
+		self.id = int(id)
 		self.description = description
 		self.last_sold = last_sold
 		self.shelf_life = shelf_life
 		self.department = department
 		self.price = price
 		self.unit = unit
-		self.x_for = x_for
+		self.x_for = int(x_for)
 		self.cost = cost
-		
-	def __repr__(self):
-		return '<id {}>'.format(self.id)
